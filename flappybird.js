d740a75a6aa04ba68e3a45a310d4b9c4b38b022d
unknown <SNK FULL@DESKTOP-26144.26144>
2024-04-22 03:40:35 -0300

    Making bird fly :)

---
+++ flappybird.js
@@ -0,0 +1,18 @@
+// board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+//bird
+let birdWidth = 34;
+let birdHeight = 24;
+let birdx = boardWidth/8;
+let birdy = boardHeight/2;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d");
+}
