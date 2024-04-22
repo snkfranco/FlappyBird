0062be307a991d844bdfdc10054e2678eaf1e626
unknown <SNK FULL@DESKTOP-26144.26144>
2024-04-22 04:24:45 -0300

    Much better..

--- flappybird.js
+++ flappybird.js
@@ -9,10 +9,105 @@ let birdWidth = 34;
 let birdHeight = 24;
 let birdx = boardWidth/8;
 let birdy = boardHeight/2;
+let birdImage;
+
+let bird = {
+    x : birdx,
+    y : birdy,
+    width : birdWidth,
+    height : birdHeight
+}
+
+// pipes
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImage;
+let bottomPipeImage;
+
+// physics
+let velocityX = -2; // pipes moving left speed
+let velocityY = 0; // bird jump speed
 
 window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d");
+
+    // draw flappy bird
+    // context.fillStyle = "green";
+    // context.fillRect(bird.x, bird.y, bird.width, bird.height);
+
+    // load images
+    birdImage = new Image();
+    birdImage.src = "./Assets/flappybird.png";
+    birdImage.onload = function() {
+        context.drawImage(birdImage, bird.x, bird.y, bird.width, bird.height);
+    }
+
+    topPipeImage = new Image();
+    topPipeImage.src = "./Assets/toppipe.png";
+
+    bottomPipeImage = new Image();
+    bottomPipeImage.src = "./Assets/bottompipe.png";
+
+    requestAnimationFrame(update);
+    setInterval(placePipes, 1500);
+    document.addEventListener("keydown", moveBird);
+}
+
+function update() {
+    requestAnimationFrame(update);
+    context.clearRect(0,0, board.width, board.height);
+
+    // bird
+    bird.y += velocityY;
+    context.drawImage(birdImage, bird.x, bird.y, bird.width, bird.height);
+
+    // pipes
+    for (let i = 0; i < pipeArray.length; i++) {
+        let pipe = pipeArray[i];
+        pipe.x += velocityX;
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+    }
+}
+
+function placePipes() {
+
+    let randomPipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2);
+    let openingSpace = board.height/4;
+
+    let topPipe = {
+        img : topPipeImage,
+        x : pipeX,
+        y : randomPipeY,
+        width : pipeWidth,
+        height : pipeHeight,
+        passed : false
+    }
+
+    pipeArray.push(topPipe);
+
+    let bottompipe = {
+        img : bottomPipeImage,
+        x : pipeX,
+        y : randomPipeY + pipeHeight + openingSpace,
+        width : pipeWidth,
+        height : pipeHeight,
+        passed : false
+    }
+
+    pipeArray.push(bottompipe);
+}
+
+function moveBird(e) {
+    if (e.code == "Space" || e.code == "ArrowUp") {
+        
+        //jump
+        velocityY = -6;
+    }
 }
